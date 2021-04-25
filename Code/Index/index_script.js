9ac0dc5a091580b67bdde5cd7dfe14483cf8b3c5
Pavel <Papaskaaaa@gmail.com>
2021-04-26 02:45:07 +0300

    intro block
    add intro block without mobile version
    change vertical align button hamburger and her border
    add background image for page

--- Code/Index/index_script.js
+++ Code/Index/index_script.js
@@ -1,21 +1,49 @@
+//hamburger
 let coordY = 0
 function hamburger(){//Проверка на состояние гамбургера
     let hamb = document.querySelector('.hamburger_active').offsetWidth
     if(hamb === 0){ //Если width === 0 то он закрыт
         let idmove = setInterval(function(){
-            coordY+=1
-            document.querySelector('.hamburger_active').style.width = coordY + "%"
+            coordY+=9
+            document.querySelector('.hamburger_active').style.width = coordY + "px"
         },1)
         setTimeout(() => clearInterval(idmove),130)
         
     }else{
         let idmove = setInterval(function(){
             if(coordY > 0){
-                coordY-=1
-                document.querySelector('.hamburger_active').style.width = coordY + "%"
+                coordY-=9
+                document.querySelector('.hamburger_active').style.width = coordY + "px"
             }
         },1)
         setTimeout(() => clearInterval(idmove),140)
     }
+}
+
+//slider team
+let current_slide = 0
+let slide = document.querySelector(".move_slider")
+slide.style.left = "0px"//фикс бага - первое перелистывание без анимации
+
+function slider_next(){
+
+    if(current_slide < 10){
+        current_slide++
+    }else{
+        current_slide = 0
+    }
+
+    slide.style.left = (-current_slide) * 52 - 6 * current_slide + "px"
+}
+
+function slider_prev(){
+
+    if(current_slide === 0){
+        current_slide = 10
+    }else{
+        current_slide--
+    }
+
+    slide.style.left = (-current_slide) * 52 - 6 * current_slide + "px"
 }
 
